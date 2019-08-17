6a1b4686d084fba8a4432eadc1487e11a3e1b0cd
SplendidX <splendidx@cocaine.ninja>
2019-08-17 17:33:10 +0300

    rest of the fetchable data

--- lib/ex_coinranking.ex
+++ lib/ex_coinranking.ex
@@ -1,37 +1,60 @@
 defmodule Excoinranking do
+  @moduledoc """
+    An Elixir wrapper for the Coinranking API, using HTTPoison and Poison.
+    example: coin(1, %{base: "USD", timePeriod: "30d"})
+
+    Coinranking API Documentation: https://docs.coinranking.com/public
+  """
+
   use HTTPoison.Base
-  use Poison
+  alias Poison
 
   @base_url "https://api.coinranking.com/v1/public/"
 
   HTTPoison.start()
 
-  def get(data) do
-    case HTTPoison.get(@base_url <> data) do
+  def get(data, query_params \\ %{base: "USD"}) do
+    case HTTPoison.get(@base_url <> data, [], params: query_params) do
       {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
         {code, body}
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts(":(")
+      {:error, %HTTPoison.Error{} = error} ->
+        error
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
+      {_, msg} ->
+        msg.body |> Poison.decode!()
     end
-    |> fn {ok, body} ->
-      body
-      |> Poison.decode(keys: :atoms)
-      |> case do
-        {:ok, parsed} -> {ok, parsed}
-        _ -> {:error, body}
-      end
-    end
+    |> (fn {ok, body} ->
+          body
+          |> Poison.decode(keys: :atoms)
+          |> case do
+            {:ok, parsed} -> {ok, parsed}
+            _ -> {:error, body}
+          end
+        end).()
   end
 
-  def get_coins do
-    get("coins")
+  def coins(query_params \\ %{}) do
+    get("coins", query_params)
   end
 
-  def get_coin_by_id(id) do
-    get("coin/#{id}")
+  def coin(id, query_params \\ %{}) do
+    get("coin/#{id}", query_params)
+  end
+
+  def coin_history(id, timeframe \\ "24h", query_params \\ %{}) do
+    get("coin/#{id}/history/#{timeframe}", query_params)
+  end
+
+  def global_stats(query_params \\ %{}) do
+    get("stats", query_params)
+  end
+
+  def markets(query_params \\ %{}) do
+    get("markets", query_params)
+  end
+
+  def exchanges(query_params \\ %{}) do
+    get("exchanges", query_params)
   end
 end
