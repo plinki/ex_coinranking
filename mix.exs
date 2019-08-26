83c29a88135f0c399b8f38ae72bdf5043d0d7951
SplendidX <splendidx@cocaine.ninja>
2019-08-26 18:01:18 +0300

    add a license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule ExCoinranking.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+
+      # ex_doc
+      name: "Coinranking",
+      source_url: "https://github.com/SplendidX/ex_coinranking",
+      docs: [
+        main: "Coinranking",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,7 +32,24 @@ defmodule ExCoinranking.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.5"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    An Elixir wrapper for the Coinranking API, using HTTPoison and Poison.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["GPL 3.0"],
+      links: %{
+        "GitHub" => "https://github.com/SplendidX/ex_coinranking"
+      }
     ]
   end
 end
